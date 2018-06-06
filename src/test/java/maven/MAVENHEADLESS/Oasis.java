90a07a369d8d6b934717ce3e72caabfe3b629520
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2018-06-06 13:29:56 -0500

    Create Oasis.java

---
+++ src/test/java/maven/MAVENHEADLESS/Oasis.java
@@ -0,0 +1,69 @@
+
+import java.util.Set;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Oasis {
+	WebDriver driver;
+	String u = "https://k12oasis.pearson.com/ordering/oaLogin.aspx";
+	
+	
+	  @Test (priority=1)
+	  public void acorn1() {
+		  Cookie name = new Cookie("LB", "acorn1");
+			driver.manage().addCookie(name);
+			driver.get(u);
+			Assert.assertEquals(driver.getTitle(), "Login");
+			 
+		  
+	  }
+	  
+
+	  
+	  
+	  @BeforeMethod
+	  public void beforeMethod() {
+		  
+		// Declaring and initialize  HtmlUnitWebDriver
+			driver = new HtmlUnitDriver();
+			driver.get(u);
+			
+	  }
+
+	  @AfterMethod
+	  public void afterMethod(ITestResult result) {
+		  
+		   
+		  
+		  Set<Cookie> cookiesList =  driver.manage().getCookies();
+			for(Cookie getcookies :cookiesList) {
+				
+				if (getcookies.getName().contains("LB")) {
+			    System.out.print(getcookies.getName() +"  "+ getcookies.getValue() + " OASIS ");
+				}else {}
+			}
+			
+			
+			if (result.getStatus() == ITestResult.SUCCESS) {
+				System.out.println("");
+			   } 
+			
+			if (result.getStatus() == ITestResult.FAILURE) {
+				System.out.println(" >>> failed ");
+			   } 
+			
+			if (result.getStatus() == ITestResult.SKIP) {
+				System.out.println(" >>> skipped ");
+			   } 
+			
+		  driver.close();
+	  }
+
+	}
